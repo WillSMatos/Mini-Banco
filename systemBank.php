4f3e7af7ada25882ca90c1d67876e6019f0dd6d3
Willian Saud <devsaud.contato@gmail.com>
2025-04-09 15:42:48 -0300

    Criado um menu interativo

---
+++ systemBank.php
@@ -0,0 +1,15 @@
+<?php
+
+$titularConta = "Willian Saud";
+$saldo = 1000;
+
+echo "-------------------------\n";
+echo "Titular: $titularConta\n";
+echo "Saldo atual: $saldo\n";
+echo "-------------------------\n";
+echo "\n";
+
+echo "1. Consultar saldo atual\n";
+echo "2. Realizar saque\n";
+echo "3. Realizar depósito\n";
+echo "4. Sair\n";
